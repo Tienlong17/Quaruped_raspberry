06c18e861e949b284d171911f69dba33c72719e7
Long Trần <91883421+Tienlong17@users.noreply.github.com>
2022-05-26 12:03:49 +0100

    Update main_module.py

--- Quadruped_project/main_module.py
+++ Quadruped_project/main_module.py
@@ -10,19 +10,19 @@ HIGH_stand = 30 #day la chieu cao dat lam gia tri khi dung
 
 # information about action of Robot
 TM_trot = 2
-s_trot_step = 10 # khoang cach sai buoc chan
-high_trot_step = 6 # do cao nhac chan len de buoc tiep
+s_step_trot = 10 # khoang cach sai buoc chan
+high_step_trot = 6 # do cao nhac chan len de buoc tiep
 
 TM_crawl = 1.5 # chu ki buoc
-s_crawl_step = 10 # khoang cach sai buoc chan
-high_crawl_step = 6 # do cao nhac chan len de buoc tiep 
+s_step__crawl = 10 # khoang cach sai buoc chan
+high_step_crawl = 6 # do cao nhac chan len de buoc tiep 
 sampling_time  = 0.05# thoi gian lay mau 
 
 
 
 def Move_Robot(type_move: int, direction: int):
-    global TM_trot, s_trot_step, high_trot_step, TM_crawl, s_crawl_step, high_crawl_step, sampling_time, L1, L2, L3, HIGH_stand
+    global TM_trot, s_step_trot, high_step_trot, TM_crawl, s_step__crawl, high_step_crawl, sampling_time, L1, L2, L3, HIGH_stand
     if type_move == 1:
-        Math_auto_quadruped.Type_Trot(direction, TM_trot, s_trot_step, high_trot_step, sampling_time, L1, L2, L3, HIGH_stand)
+        Math_auto_quadruped.Type_Trot(direction, TM_trot, s_step_trot, high_step_trot, sampling_time, L1, L2, L3, HIGH_stand)
     if type_move == 2:
-        Math_auto_quadruped.Type_Crawl(direction, TM_crawl, s_crawl_step, high_crawl_step, sampling_time, L1, L2, L3, HIGH_stand)
+        Math_auto_quadruped.Type_Crawl(direction, TM_crawl, s_step__crawl, high_step_crawl, sampling_time, L1, L2, L3, HIGH_stand)
