957325de8b7157cad570fe8a27089ec8e1f6f0d9
Long Trần <91883421+Tienlong17@users.noreply.github.com>
2022-12-12 15:53:01 +0000

    crawl ok nhung trot can fix

--- tempt_11/Interface_graphics.py
+++ tempt_11/Interface_graphics.py
@@ -31,106 +31,93 @@ color_button1 = (232, 53, 21)
 color_button2 = (232, 232, 21)
 blue_light = (21, 232, 204)
 blue_drark = (2, 38, 102)
-
-huong =0
-isClick = 0
-typeMove = 0
-def Create_Text_Word(a : str):
+direction = -1
+typeMove = 2
+isStand = 0
+def Create_Text_Word(a : str, color):
     '''Ham de tao ghi chu~'''
     font = pygame.font.SysFont("sans",30)
-    return  font.render(a,True, blue_light)
-def Create_Button(name: str, position_x: float,position_y: float):
-    text = Create_Text_Word(name)
-    text_box = text.get_rect()
-    pygame.draw.rect(screen,blue_drark,(position_x,position_y,text_box[2],text_box[3]))
-    screen.blit(text, (position_x,position_y))
-    if is_mouse_on_text(position_x, position_y, text_box[2],text_box[3]):
-        pygame.draw.rect(screen,(100,100,100), (position_x,position_y,text_box[2],text_box[3]))
-        pygame.draw.line(screen, (0,0,255), (position_x, position_y + text_box[3]), (position_x +text_box[2], position_y + text_box[3]))
-        screen.blit(text, (position_x,position_y))
-
-def is_mouse_on_text(position_x,position_y,position_z,position_t):
-        mouse_x, mouse_y = pygame.mouse.get_pos()
-        if position_x < mouse_x < position_x + position_z and position_y < mouse_y < position_y + position_t:
-            return True
-        else:
-            return False
-        
-def Click_Button():
-    text1 = Create_Text_Word(' Trotting ')
-    text_box1 = text1.get_rect()
-    position_x1,position_y1 = 200,42
-    text2 = Create_Text_Word(' Crawling ')
-    text_box2 = text2.get_rect()
-    position_x2,position_y2 = 320,42
-    global typeMove
-    if position_x1 < mouse_x < position_x1 + text_box1[2] and position_y1 < mouse_y < position_y1 + text_box1[3]:
-        typeMove = 1
-    if position_x2 < mouse_x < position_x2 + text_box2[2] and position_y2< mouse_y < position_y2 + text_box2[3]:
-        typeMove = 2
+    return  font.render(a,True, color)
 
 clock = pygame.time.Clock()
+try:
+    while running:
+        clock.tick(60) # 60
+        screen.fill(Color_BackGorund)
 
-def Get_Button(mouse_x,mouse_y):
-    x = 0
-    if (120 < mouse_x < 170) and (350 < mouse_y < 400):
-        x = 1
+        mouse_x,mouse_y = pygame.mouse.get_pos()
+        '''Giao dien man hinh'''
+        # button  moving function 
+        pygame.draw.rect(screen,color_button2,(120,350,50,50))
+        pygame.draw.rect(screen,color_button1,(120,430,50,50))
+        pygame.draw.rect(screen,color_button2,(120,500,50,50))
+        pygame.draw.rect(screen,color_button2,(50,430,50,50))
+        pygame.draw.rect(screen,color_button2,(190,430,50,50))
 
-    if (120 < mouse_x < 170) and (500 < mouse_y < 550):
-        x = 2
+        #draw information
+        screen.blit(Create_Text_Word('Name Robot:',blue_light),(12,12))
+        screen.blit(Create_Text_Word('Quadruped',blue_light),(200,12))
+        screen.blit(Create_Text_Word('Walking Style:',blue_light),(11,42))
+        
+        # draw funtion type of movement
+        if typeMove == 1:
+            pygame.draw.rect(screen,color_button2,(205,45,150,27))
+            screen.blit(Create_Text_Word('1.Trotting',blue_drark),(205,45))
+            screen.blit(Create_Text_Word('2.Crawling',blue_light),(360,45))
+        elif typeMove == 2:
+            screen.blit(Create_Text_Word('1.Trotting',blue_light),(205,45))
+            pygame.draw.rect(screen,color_button2,(360,45,150,27))
+            screen.blit(Create_Text_Word('2.Crawling',blue_drark),(360,45))
+        elif 0< typeMove < 3:
+            screen.blit(Create_Text_Word('1.Trotting',blue_light),(205,45))
+            screen.blit(Create_Text_Word('2.Crawling',blue_light),(360,45))
+        direction = -1 
+        for event in pygame.event.get():
+            # Event by Type of walking Keyboard kieu di 
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_1:
+                    typeMove = 1 # di kieu trot 
+                if event.key == pygame.K_2:
+                    typeMove = 2 # di kieu crawl 
+                # Movement Function 
+                if event.key == pygame.K_LEFT: 
+                    direction = 4
+                if event.key == pygame.K_RIGHT: 
+                    direction = 2
+                if event.key == pygame.K_UP: 
+                    direction = 1
+                if event.key == pygame.K_DOWN: 
+                    direction = 3
+                if event.key == pygame.K_a:         # xoay trai 
+                    main_module.Spin_Left()
+                if event.key == pygame.K_d:         # xoay phai
+                    main_module.Spin_Right()
+                if event.key == pygame.K_i:         # dua ve vi tri nam
+                    main_module.Default_0_degree()
+                    isStand = 0
+                if event.key == pygame.K_u:         # dua ve vi tri di dung
+                    main_module.Default_Stand_Up()
+                if event.key == pygame.K_o:         # dung len
+                    main_module.Stand_Robot(isStand)
+                    isStand = 1
+                if event.key == pygame.K_p:         # nam xuong 
+                    main_module.Down_Robot(isStand)
+                    isStand = 2
+        if 0< typeMove <3: # khong truyen so khong thi co di chuyen khong 
+            #print('typeMove =',typeMove) Ve giao dien dang di theo kieu nao
+            if 0 <= direction < 7:
+                #direction = main_module.Check_Object(direction)
+                main_module.Move_Robot(typeMove,direction) # 60s 
+        
+        if event.type == pygame.QUIT:
+            running = False
+            print('Exit program')
+            main_module.GPIO.cleanup()# giai phong bo nho GPIO
 
-    if (50 < mouse_x < 100) and (430 < mouse_y < 480):
-        x = 3
+        pygame.display.flip()
 
-    if (190 < mouse_x < 240) and (430 < mouse_y < 480):
-        x = 4
-
-    if (120 < mouse_x < 170) and (430 < mouse_y < 480):
-        x = 0
-    return x
-
-while running:
-    clock.tick(60) # 60
-    screen.fill(Color_BackGorund)
-
-    mouse_x,mouse_y = pygame.mouse.get_pos()
-    '''Giao dien man hinh'''
-    # button  moving function 
-    pygame.draw.rect(screen,color_button2,(120,350,50,50))
-    pygame.draw.rect(screen,color_button1,(120,430,50,50))
-    pygame.draw.rect(screen,color_button2,(120,500,50,50))
-    pygame.draw.rect(screen,color_button2,(50,430,50,50))
-    pygame.draw.rect(screen,color_button2,(190,430,50,50))
-
-    #draw information
-    screen.blit(Create_Text_Word('Name Robot:'),(12,12))
-    screen.blit(Create_Text_Word('Quadruped'),(200,12))
-    screen.blit(Create_Text_Word('Walking Style:'),(11,42))
-    
-    # draw button
-    Create_Button(' Trotting ',200,42)
-    Create_Button(' Crawling ',320,42)
-    for event in pygame.event.get():
-        if event.type == pygame.MOUSEBUTTONDOWN:
-            isClick = 1
-        if event.type == pygame.MOUSEBUTTONUP:
-            isClick = 0
-
-    if isClick == 1:
-        huong = Get_Button(mouse_x,mouse_y)
-        Click_Button()
-    else:
-        huong = 0
-
-    print('huong',huong)
-    print('typeMove',typeMove)
-    main_module.Move_Robot(typeMove,huong) # 60s 
-
-
-    
-    if event.type == pygame.QUIT:
-        running = False
-
-    pygame.display.flip()
-
-pygame.quit()
+    pygame.quit()
+except KeyboardInterrupt:
+    print('Interupt Ctrl + C')
+    main_module.GPIO.cleanup()#giai phong bo nho GPIO
+    print('Exit program')
